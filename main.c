e09b42cb7b98f46958bc4541f0a2b4924a6cde16
Deangibson19 <111289865+Deangibson19@users.noreply.github.com>
2023-03-22 15:58:01 +0200

    main.c for all questions of prac5

--- main.c
+++ main.c
@@ -14,25 +14,63 @@ void display_on_LEDs(uint8_t count);
 void init_switches(void);
 void delay(unsigned int microseconds);
 int update_count(int count);
+void init_external_interrupts(void);
 
+uint8_t SW3_count = 0;
 
 
 // MAIN FUNCTION -------------------------------------------------------------|
-
 void main(void)
 {
 	init_LEDs();
 	init_LCD();
 	init_switches();
+	init_external_interrupts();
 
-	int count = 0;
+	uint8_t count = 0;
 
 	while(1)
 	{
-		count= update_count(count);
-		delay(20000);
-		display_on_LCD(count);
-		display_on_LEDs(count);
+		if ((SW3_count%2) != 0)
+		{
+			if ((GPIOA->IDR & GPIO_IDR_1) == 0)
+			{
+				if (count < 255)
+				{
+					count++;
+				}
+				//else
+				//{
+					//count = count;
+				//}
+				lcd_command(CLEAR);
+				display_on_LCD(count);
+				display_on_LEDs(count);
+				delay(50000);
+			}
+			else if ((GPIOA->IDR & GPIO_IDR_2) == 0)
+			{
+				if (count <= 0)
+				{
+					count = count;
+				}
+				else
+				{
+					count--;
+				}
+				lcd_command(CLEAR);
+				display_on_LCD(count);
+				display_on_LEDs(count);
+				delay(50000);
+			}
+		}
+		else
+		{
+			count = 0;
+			lcd_command(CLEAR);
+			display_on_LEDs(count);
+		}
+
 	}
 
 }
@@ -48,8 +86,6 @@ void display_on_LCD(unsigned char count)
 	delay(20000);
 
 }
-
-
 
 void init_LEDs(void)
 {
@@ -72,31 +108,54 @@ void init_switches(void)
 {
 	RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;
 
-	GPIOA -> MODER &= ~GPIO_MODER_MODER0;
 	GPIOA -> MODER &= ~GPIO_MODER_MODER1;
-	GPIOA -> PUPDR |= GPIO_PUPDR_PUPDR0_0;
+	GPIOA -> MODER &= ~GPIO_MODER_MODER2;
+	GPIOA -> MODER &= ~GPIO_MODER_MODER3;
 	GPIOA -> PUPDR |= GPIO_PUPDR_PUPDR1_0;
+	GPIOA -> PUPDR |= GPIO_PUPDR_PUPDR2_0;
+	GPIOA -> PUPDR |= GPIO_PUPDR_PUPDR3_0;
 }
 
-const int COUNT_MIN = 0;
-const int COUNT_MAX = 255;
+//const int COUNT_MIN = 0;
+//const int COUNT_MAX = 255;
 
-int update_count(int count)
+//int update_count(int count)
+//{
+//    // increment count if SW1 is pressed and count is less than COUNT_MAX
+//    if((( (GPIOA -> IDR) &= GPIO_IDR_1)==0) && count < COUNT_MAX)
+//    {
+//        (count++);
+//    }
+//    // decrement count if SW2 is pressed and count is greater than COUNT_MIN
+//    if((( (GPIOA -> IDR) &= GPIO_IDR_2)==0) && count > COUNT_MIN)
+//    {
+//        (count)--;
+//    }
+//    if((( (GPIOA -> IDR) &= GPIO_IDR_3)==0) && count < 255)
+//       {
+//           count = count;
+//       }
+//    return count;
+//}
+
+void init_external_interrupts(void)
 {
-    // increment count if SW1 is pressed and count is less than COUNT_MAX
-    if((( (GPIOA -> IDR) &= GPIO_IDR_0)==0) && count < COUNT_MAX)
-    {
-        (count++);
-    }
-    // decrement count if SW2 is pressed and count is greater than COUNT_MIN
-    if((( (GPIOA -> IDR) &= GPIO_IDR_1)==0) && count > COUNT_MIN)
-    {
-        (count)--;
-    }
-    return count;
+	 RCC->APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;
+
+	    SYSCFG->EXTICR[2] |= SYSCFG_EXTICR1_EXTI3_PA;
+	    EXTI->IMR |= EXTI_IMR_MR3;
+	    EXTI->FTSR |= EXTI_FTSR_TR3;
+
+	    NVIC_EnableIRQ(EXTI2_3_IRQn);
 }
 
+void EXTI2_3_IRQHandler(void)
+{
+	SW3_count +=1;
+	EXTI -> PR |= EXTI_PR_PR3;
+	delay(50000);
 
+}
 
 
 
